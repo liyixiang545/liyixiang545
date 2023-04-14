405e8ff2c6500910959999bbeb001a82706ded3b
liyixiang545 <liyixiang545@gmail.com>
2023-04-14 18:52:07 +0800

    优化

--- def_function/capture_picture.py
+++ def_function/capture_picture.py
@@ -2,6 +2,8 @@ import time
 import requests
 import pymysql
 from lxml import etree
+import os
+from threading import Thread
 
 
 def get_response(url):
@@ -24,6 +26,7 @@ def getpage_html(url_text):
     try:
         html = etree.HTML(url_text)
         page_info = html.xpath("//a[@class='preview']/@href")
+
         # print(page_info)
     except:
         print("the function is faild !")
@@ -45,9 +48,13 @@ def deal_pageinfo(page_url, page_url_all):
                 continue
             else:
                 page_res.encoding = 'utf-8'
+                page_name = []
                 page_html = etree.HTML(page_res.text)
                 photo_address = page_html.xpath("//img[@id='wallpaper']/@src")
+                photo_name = page_html.xpath("//img[@id='wallpaper']/@alt")
                 page_url_all.extend(photo_address)
+                page_name.extend(photo_name)
+                print(page_name)
     except Exception as e:
         print("deal_pageinfo exits error",e)
     return page_url_all
@@ -65,23 +72,23 @@ def save_photo(page_url_all, save_path, i):
             if img.status_code != 200:
                 print("{0}不能获取...".format(x))
                 continue
-            # if int(len(page_url_all)/a) == 2 :
-            #     print("已经完成50%...")
+            if int(len(page_url_all)/a) == 2 :
+                print("已经完成50%...")
             print("正在下载第{0}张照片...".format(a))
             with open(save_path+str("picture_")+str(a)+'.jpg', 'wb') as f:
                 f.write(img.content)
             a += 1
         print("图片下载完成...")
         f.close
-    except:
-        print('图片获取失败...')
+    except Exception as e :
+        print('图片获取失败...由于{0}'.format(e))
 
 
 def connection_sql():
     '''
     连接数据库的的函数
     '''
-    conn = pymysql.connect(host='localhost', user='root', password='password', database='world')
+    conn = pymysql.connect(host='localhost', user='root', password='1qazCDE#5tgb', database='lywz')
     cur = conn.cursor()
     sql = "SELECT Population, Name FROM city\
             WHERE Population > 2000000;"
@@ -97,8 +104,12 @@ def main():
     主函数，定义全局变量，调用函数...
     '''
     time_1 = time.time()
-    # save_path = 'C:\\Users\\NaOH\\Desktop\\photo\\'
-    save_path = '\\capture_img\\'
+    save_path = '../capture_img/img/' #方法3
+    # save_path = '\\capture_img\\' #方法1
+    # save_path = '..\\capture_img\\img\\' #方法2
+    if os.path.exists(save_path) == False:
+        os.makedirs(save_path)
+
     # 保存地址可自定义
     print("注意：若输入较大的数值可能会使得最终结果失败，会耗费较大的电脑性能和网络，请合理选择数值...")
     page_end = int(input("请输入想要爬取的页数："))
@@ -110,9 +121,12 @@ def main():
         page_url = getpage_html(url_text)
         page_url_all = deal_pageinfo(page_url, page_url_all=page_url_all)
     # 把带图片链接的list 传入save_photo方法中 去保存图片
-    save_photo(page_url_all, save_path, i)
+    # t = Thread(target=save_photo(page_url_all, save_path, i))
+    # t.start()
+    # t.join()
+    # save_photo(page_url_all, save_path, i)
     time_2 = time.time() - time_1
-    print("时长为：", int(time_2))
+    print("时长为：{0}".format(int(time_2)))
 
 
 if __name__ == "__main__":
